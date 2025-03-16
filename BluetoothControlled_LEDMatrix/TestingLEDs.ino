1b0d8a347aebdb33d92c5bf4cee21e8fc06cb4e2
Katherine2 <katherine.sirois.2@gmail.com>
2025-03-16 16:58:18 +0100

    Fixed indents

--- BluetoothControlled_LEDMatrix/TestingLEDs.ino
+++ BluetoothControlled_LEDMatrix/TestingLEDs.ino
@@ -6,22 +6,22 @@
 CRGB leds[NUM_LEDS];
 
 void setup() {
-FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-// Set all LEDs to red
-fill_solid(leds, NUM_LEDS, CRGB::Red);
-FastLED.show();
-delay(1000);
-
-// Set all LEDs to green
-fill_solid(leds, NUM_LEDS, CRGB::Green);
-FastLED.show();
-delay(1000);
-
-// Set all LEDs to blue
-fill_solid(leds, NUM_LEDS, CRGB::Blue);
-FastLED.show();
-delay(1000);
+  // Set all LEDs to red
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(1000);
+  
+  // Set all LEDs to green
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(1000);
+  
+  // Set all LEDs to blue
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+  delay(1000);
 }
